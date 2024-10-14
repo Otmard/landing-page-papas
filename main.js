f2a32d8abe63fc4dfcb692b895fddd0905238c33
DESKTOP-J4DM5UA\Otmars <ootmar1998@gmail.com>
2024-10-14 12:21:20 -0400

    animaciones y traducido

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import "./style.css";
 import Swiper from "swiper";
 import "swiper/css";
 import { Autoplay } from "swiper/modules";
+import { ScrollReveal } from "./scripts/scrollreveal.min.js";
+
 const navMenu = document.getElementById("nav-menu");
 const navClose = document.getElementById("nav-close");
 const navTogle = document.getElementById("nav-togle");
@@ -80,3 +82,23 @@ const scrollActive = () => {
 	});
 };
 window.addEventListener("scroll", scrollActive);
+
+const sr = ScrollReveal({
+	origin: "top",
+	distance: "60px",
+	duration: 2500,
+	delay: 300,
+	reset:true
+});
+
+sr.reveal(".home__data, .favorites__container, .footer__container");
+sr.reveal(".home__circle, .home__img", { delay: 600, scale: 0.5 });
+sr.reveal(".home__chips-1, .home__chips-2, .home__chips-3", {
+	delay: 100,
+	interval: 100,
+});
+sr.reveal(".home__leaf", { delay: 100, interval: 100 });
+sr.reveal(".home__tomato-1, .home__tomato-2", { delay: 100, interval: 100 });
+sr.reveal(".care__img, .contact__img", { origin: "left" });
+sr.reveal(".care__list, .contact__data", { origin: "right" });
+sr.reveal(".banner__item, .products__card", { interval: 100 });
